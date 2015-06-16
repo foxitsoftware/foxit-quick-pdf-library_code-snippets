2c40fc7d6ad8528e2eaf8517023e48bdc01c9f82
Rowan Hanna <rowan.hanna@gmail.com>
2015-06-16 11:10:43 +0200

    Create extract_text_from_defined_area.cs
    C# sample code for using DASetTextExtractionArea function to specify a rectangular area on the page to extract text from.

---
+++ extract_text_from_defined_area.cs
@@ -0,0 +1,9 @@
+// C# sample code, this does not demonstration the initialization and unlocking of library
+
+int r1 = DPL.DAOpenFile(@"dev_guide.pdf", ""); // Load sample file
+int r2 = DPL.DAFindPage(r1, 2); // Second parameter is page to use for text extraction
+double ph = DPL.DAGetPageHeight(r1, r2); // Get page height
+double pw = DPL.DAGetPageWidth(r1, r2); // Get get width
+DPL.DASetTextExtractionArea(0, ph, pw, ph); // Adjust text extraction area here, currently set to full page
+string r3 = DPL.DAExtractPageText(r1, r2, 3); // Extract the text
+System.IO.File.WriteAllText(@"extracted_text.txt", r3); // Save text file with output
