a1b6a284a1f62863185ff6db2e57ec5cc6fa2353
Rowan Hanna <rowan.hanna@gmail.com>
2015-08-05 14:56:14 +0200

    Update extract_text_from_defined_area.cs
    Added better usage examples

--- extract_text_from_defined_area.cs
+++ extract_text_from_defined_area.cs
@@ -1,9 +1,23 @@
 // C# sample code, this does not demonstrate the initialization and unlocking of Debenu Quick PDF Library
 
-int r1 = DPL.DAOpenFile(@"dev_guide.pdf", ""); // Load sample file
-int r2 = DPL.DAFindPage(r1, 2); // Second parameter is page to use for text extraction
-double ph = DPL.DAGetPageHeight(r1, r2); // Get page height
-double pw = DPL.DAGetPageWidth(r1, r2); // Get get width
-DPL.DASetTextExtractionArea(0, ph, pw, ph); // Adjust text extraction area here, currently set to full page
-string r3 = DPL.DAExtractPageText(r1, r2, 3); // Extract the text
-System.IO.File.WriteAllText(@"extracted_text.txt", r3); // Save text file with output
+// SetTextExtractionArea with GetPageText
+
+DPL.LoadFromFile(@"Sample.pdf", "");
+DPL.SetOrigin(1); // Sets 0,0 coordinate position to top left of page, default is bottom left
+DPL.SetTextExtractionArea(35, 35, 229, 30); // Left, Top, Width, Height
+string ExtractedContent = DPL.GetPageText(8);
+Console.WriteLine(ExtractedContent);
+
+// DASetTextExtractionArea with ExtractFilePageText
+
+DPL.DASetTextExtractionArea(35, 757, 229, 30); // Left, Top, Width, Height
+ExtractedContent = DPL.ExtractFilePageText(@"Sample.pdf", "", 1, 8);
+Console.WriteLine(ExtractedContent);
+
+// DASetTextExtractionArea with DAExtractPageText
+
+int fileHandle = DPL.DAOpenFile(@"Sample.pdf", "");
+int pageRef = DPL.DAFindPage(fileHandle, 1);
+DPL.DASetTextExtractionArea(35, 757, 229, 30); // Left, Top, Width, Height
+ExtractedContent = DPL.DAExtractPageText(fileHandle, pageRef, 8);
+Console.WriteLine(ExtractedContent);
